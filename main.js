e6e35e06b61cbc0648d749f46ac19ba01bcadcbf
Valdruide <tviardprimo190@gmail.com>
2021-11-25 15:10:52 +0100

    rm test js

--- main.js
+++ main.js
@@ -17,21 +17,3 @@ btnMail.addEventListener("click", () => {
   badMail.textContent = "Invalid Mail !"
   return false;
 })
-
-
-
-const questionContainer = document.querySelector(".click-event");
-const btn1 = document.querySelector("#btn-1");
-const btn2 = document.getElementById("btn-2");
-const response = document.getElementById("reponse");
-
-
-btn1.addEventListener("click", () => {
-  response.style.visibility = "visible";
-  response.style.color = "green";
-})
-
-btn2.addEventListener("click", () => {
-  response.style.visibility = "visible";
-  response.style.color = "red";
-})
