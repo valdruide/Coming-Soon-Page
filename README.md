540fefde6a69dc894095115a301414bb9efee074
Valdruide <tviardprimo190@gmail.com>
2021-11-25 14:48:51 +0100

    update JS + responsive

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Coming soon page + Newsletter subscription
 
 Réalisation d'une page Coming soon avec newsletter(JS)
+Script JS ==> Check if input is valid mail
 
 ## À faire :
-    - Faire le JS pour valider l'email
+
     - Rendre le site responsive
 
 
 ## Overview
 
-Réalisation afin perfectionner mon JS, Bootstrap et continuer d'apprendre à rendre le tout responsive.
+Réalisation afin de perfectionner mon JS, Bootstrap et continuer d'apprendre à rendre le tout responsive.
 
 
 ### Screenshots
--- main.js
+++ main.js
@@ -1,10 +1,37 @@
-function ValidateEmail(mail) 
-{
- if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(myForm.emailAddr.value))
-  {
-    alert("You have entered an valid email address!")
-    return (true)
+const btnMail = document.getElementById("btn-mail");
+const badMail = document.getElementById("badMail");
+
+btnMail.addEventListener("click", () => {
+  if (
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+      emailAddr.value
+    )
+  ) {
+    badMail.style.visibility = "visible";
+    badMail.style.color = "green";
+    badMail.textContent = "Success !"
+    return true;
   }
-    alert("You have entered an invalid email address!")
-    return (false)
-}
+  badMail.style.visibility = "visible";
+  badMail.style.color = "red";
+  badMail.textContent = "Invalid Mail !"
+  return false;
+})
+
+
+
+const questionContainer = document.querySelector(".click-event");
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const response = document.getElementById("reponse");
+
+
+btn1.addEventListener("click", () => {
+  response.style.visibility = "visible";
+  response.style.color = "green";
+})
+
+btn2.addEventListener("click", () => {
+  response.style.visibility = "visible";
+  response.style.color = "red";
+})
